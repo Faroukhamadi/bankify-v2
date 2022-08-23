37090ee5c44f13715d53e485719765a9697b4a1f
Faroukhamadi <faroukhamadi1@outlook.com>
2022-08-23 20:05:09 +0100

    containerize with bugs

--- server/start-local.sh
+++ server/start-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run dev & npm run start-service-transaction-dev & npm run start-service-withdraw-dev & npm run start-service-deposit-dev & npm run start-service-transfer-dev 
+npm run dev & npm run start-service-transaction-dev & npm run start-service-withdraw-dev & npm run start-service-deposit-dev & npm run start-service-transfer-dev & npm run watch
